bf6cc741ca56e1124854ac04873dd7a181be72d0
Antti Jankeri <antti.jankeri@appliedtechnology.se>
2023-11-02 16:48:23 +0100

    fix: build it bob

--- dist/routes/mobs.js
+++ dist/routes/mobs.js
@@ -5,14 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-const mobs = [{ name: "Rasor", id: 1 }];
+const mobs = [{ name: "Rasor", id: Date.now() }];
 router.get("/", function (req, res) {
     res.json(mobs);
 });
 router.post("/", function (req, res) {
-    const body = req.body;
+    const body = { name: req.body.name, id: Date.now() };
     mobs.push(body);
     res.status(201).json(body);
-    console.log(body);
 });
 exports.default = router;
