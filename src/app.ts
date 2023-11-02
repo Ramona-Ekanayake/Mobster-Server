984b7471ee6d62f4cd036f6db29b17966e3bd2e4
Antti Jankeri <antti.jankeri@appliedtechnology.se>
2023-11-02 16:04:22 +0100

    fix build + cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-// import cors from "cors";
+import cors from "cors";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -10,7 +10,7 @@ import mobsRouter from "./routes/mobs";
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
